dac68ab22153f94be8ef4e5802a15c656f6ad983
Hao Zhou <hzhou@tripadvisor.com>
2017-02-07 17:25:04 -0500

    annotation fix

--- src/main/java/me/hzhou/JerseyConfig.java
+++ src/main/java/me/hzhou/JerseyConfig.java
@@ -3,11 +3,14 @@ package me.hzhou;
 import me.hzhou.resource.HelloWorldResource;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.wadl.internal.WadlResource;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by hzhou on 2/7/17.
  * Email: i@hzhou.me
  */
+
+@Component
 public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
